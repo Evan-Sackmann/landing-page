6f76e81e1900d3104230fd9d08d027fefea568c2
Evan-Sackmann <100897393+Evan-Sackmann@users.noreply.github.com>
2022-10-06 00:26:02 -0700

    basic layout

---
+++ src/components/Homepage.js
@@ -0,0 +1,360 @@
+import React from "react";
+import mobileAppDesign from "../img/mobile-app-design-and-development.png";
+import websiteDesign from "../img/website-design-and-development.png";
+import blockchainDev from "../img/blockchain-development.png";
+import businessAndEnterprise from "../img/business-and-enterprise-tools.png";
+import customSoftware from "../img/custom-software-development.png";
+import canada from "../img/canada.png";
+import flexibility from "../img/financial-planning.png";
+import shackhacker from "../img/shackhacker.png";
+import squarewon from "../img/square-won.png";
+import fraserland from "../img/fraserland-organics.jpeg";
+
+export default function Homepage() {
+	return (
+		<main className="main">
+			<header className="header">
+				<ul>
+					<li>Home</li>
+					<li>People</li>
+					<li>Portfolio</li>
+					<button>Get in touch</button>
+				</ul>
+			</header>
+			<div className="home">
+				<section className="hero">
+					<h1>Custom tech solutions for your business.</h1>
+					<p>
+						Frame One Software provides businesses all across the
+						world with affordable websites, mobile apps, and
+						business tools in a variety of industries.
+					</p>
+					<button>Get a free consultation</button>
+				</section>
+				<section className="company-info">
+					<h2>What We Do</h2>
+					<div className="info-container">
+						<div className="info-item">
+							<img src={mobileAppDesign} alt="" />
+							<h5>Mobile App Design & Development</h5>
+							<p>
+								Our team boasts a rich background in developing
+								and delivering cross-platform mobile apps for
+								both iOS & Android. We help with every step of
+								this process, including designing & prototyping,
+								implementing backend business logic, creating
+								seamless & consistent user experiences,
+								configuring in-app purchases & revenue streams,
+								and most importantly making sure your app gets
+								released on time without problems.
+							</p>
+						</div>
+						<div className="info-item">
+							<img src={websiteDesign} alt="" />
+
+							<h5>Web Design & Development</h5>
+							<p>
+								Does your company have a website? Does it work?
+								Looking to offer users a robust web-app to
+								supplement your online presence? Well, that's
+								where Frame One comes in. Collaborate with our
+								designers to find an aesthetic that fits your
+								business while our developers implement all the
+								features necessary to meet your business
+								requirements.
+							</p>
+						</div>
+						<div className="info-item">
+							<img src={blockchainDev} alt="" />
+
+							<h5>Blockchain Development</h5>
+							<p>
+								Whether your business is taking a step into the
+								metaverse, or you want to develop smart
+								contracts on the blockchain, Frame One Software
+								has the expertise to make it happen. Frame One
+								Software specializes in working with existing
+								blockchains including Ethereum, Bitcoin, Solana,
+								Avalanche, Polygon, Arbitrum, and many more. We
+								can create robust DAPPs and enterprise level
+								solutions that allow you to utilize the newest
+								technologies in the space.
+							</p>
+						</div>
+						<div className="info-item">
+							<img src={businessAndEnterprise} alt="" />
+
+							<h5>Business & Enterprise Tools</h5>
+							<p>
+								We excel at creating tailor-made internal tools
+								for businesses of all sizes. Whether you need a
+								platform to help track & improve employee
+								productivity, or need your existing system to
+								support integration with your partners or third
+								parties, we've got you covered. Our offerings in
+								this category can meet your exact needs,
+								including mobile application, web-based
+								solutions, and custom apis.
+							</p>
+						</div>
+						<div className="info-item">
+							<img src={customSoftware} alt="" />
+
+							<h5>Custom Software</h5>
+							<p>
+								Don't see something that fits the bill? Have
+								something else in mind? Many of our clients
+								create solutions that cross into many different
+								technology paradigms. Please reach out for a
+								chat, and we'll see where we align and how we
+								can help you realize your objectives.
+							</p>
+						</div>
+					</div>
+				</section>
+				<section>
+					<h2>Why Choose Us?</h2>
+					<div className="info-container">
+						<div className="info-item">
+							<img src={canada} alt="" />
+							<h5>Trusted Onshore Developers</h5>
+							<p>
+								Our staff is entirely based in Canada. Unlike
+								many other software development shops that
+								simply outsource projects and utilize overseas
+								contractors, we hire and train all of our
+								developers in-house. Frame One Software
+								painstakingly trains each of its members to
+								maintain a high level of quality and
+								consistency. Coding practices remain consistent
+								throughout all of our projects and everyone
+								involved has a robust and well-defined skillset.
+								We maintain our high level of quality, by
+								creating a team of developers who are
+								comfortable working with each other and are able
+								to consistently deliver high quality products to
+								our clients.
+							</p>
+							<p>Check out our team</p>
+						</div>
+						<div className="info-item">
+							<img src={flexibility} alt="" />
+							<h5>Flexibility and Affordability</h5>
+							<p>
+								Frame One Software understands the modern day
+								start-up. We know what it's like to bootstrap a
+								business, because we've done it ourselves and
+								helped dozens of our clients do the same. We
+								offer a unique approach to payment that allows
+								you to grow your business and product without
+								breaking the bank. We offer affordable payment
+								plans, where you can pay a fixed amount monthly
+								until your bills are paid off, rather than pay a
+								lump sum. This allows you to use the revenue you
+								earn from your product to pay for the building
+								of it. Our clients have utilized this strategy
+								to build businesses with minimal initial
+								investment.
+							</p>
+							<p>Discuss payment options</p>
+						</div>
+					</div>
+				</section>
+				<section>
+					<div className="case-study-container">
+						<h2>Case Studies</h2>
+						<div className="case-study">
+							<div className="case-study-info">
+								<h4>ShackHacker</h4>
+								<div className="container">
+									<span>Mobile App</span>
+									<span>Productivity</span>
+									<span>In the Field</span>
+								</div>
+								<p>
+									We worked with GoldSpot Discoveries to help
+									them develop a cross-platform mobile app to
+									boost productivity for employees of mining
+									companies. The app allows representatives to
+									sign up on behalf of their company, and
+									configure details such as team members,
+									projects, and drill sites. Users within a
+									company are divided into several categories,
+									each with their own permissions and
+									abilities. All of this is controlled and
+									monitored in a robust admin portal we
+									created for the client.
+								</p>
+								<p>View our full portfolio</p>
+							</div>
+							<img src={shackhacker} alt="" />
+						</div>
+						<div className="case-study">
+							<div className="case-study-info">
+								<h4>SquareWon</h4>
+								<div className="container">
+									<span>Website</span>
+									<span>
+										Web3, Blockchain & Cryptocurrency
+									</span>
+									<span>Live Events</span>
+								</div>
+								<p>
+									SquareWon is a start-up entering the web3
+									space with the vision of making
+									cryptocurrency investment less scary and
+									more accessible. They are establishing a
+									robust suite of educational content centered
+									around a fantasy crypto-currency portfolio
+									system. Frame One Software built this
+									extensive platform which allows users to
+									manage a cryptocurrency portfolio using real
+									market values in a safe fantasy environment.
+									We built a system that hosts contests and
+									competitions, to allow users to test trading
+									strategies in a safe and secure way, without
+									the need for an extensive knowledge of
+									obscure wallet and blockchain tools
+								</p>
+								<p>View our full portfolio</p>
+							</div>
+
+							<img src={squarewon} alt="" />
+						</div>
+						<div className="case-study">
+							<div className="case-study-info">
+								<h4>Fraserland Organics</h4>
+								<div className="container">
+									<span>Mobile App</span>
+									<span>Business Tools</span>
+									<span>Productivity</span>
+								</div>
+								<p>
+									Frame One Software developed the Fraserland
+									Organics companion app for their business.
+									The app is installed on employees devices
+									and allows them to record farming related
+									data collection & management activities. Our
+									application has completely modernized
+									Fraserland Organics and has allowed their
+									farm to gain a competitive edge by
+									implementing technology into their work
+									system. The application provides important
+									features such as maps, data viewing, data
+									submission, and a news feed of activity of
+									employees, all being managed by a central
+									admin portal custom built to meet the needs
+									of the business.
+								</p>
+								<p>View our full portfolio</p>
+							</div>
+							<img src={fraserland} alt="" />
+						</div>
+						<button>View our full portfolio</button>
+					</div>
+				</section>
+				<section>
+					<h2>Affordable Pricing & Financing</h2>
+					<p>
+						We've worked with business of all sizes - small, medium,
+						and large. Because of this, we understand better than
+						anyone the importance of offering affordable prices for
+						our clients. Choose from any of our flexible plans, or
+						give us a call and see what works best for you.
+					</p>
+					<div>
+						<h5>Hourly Contract</h5>
+						<p>
+							Our most frequent form of engagement. Pay as you go
+							- and only for what you need.
+						</p>
+						<ul>
+							<li>Free estimate</li>
+							<li>Flexible scope</li>
+							<li>Close collaboration with our team members</li>
+							<li>On-going development & support</li>
+						</ul>
+						<button>Get started</button>
+					</div>
+					<div>
+						<h5>Fixed Contract</h5>
+						<p>
+							Perfect if you've got a very defined vision in mind
+							with a clear goal to reach.
+						</p>
+						<ul>
+							<li>Free quote</li>
+							<li>Static scope means faster delivery</li>
+							<li>
+								On-going development & support negotiable after
+								delivery
+							</li>
+						</ul>
+						<button>Get started</button>
+					</div>
+					<div>
+						<h5>Payment Plans</h5>
+						<p>
+							Don't have the capital up front? We offer payment
+							plans so you are able to grow your business without
+							breaking the bank.
+						</p>
+						<ul>
+							<li>Free estimate</li>
+							<li>Have your product pay for itself</li>
+							<li>Monthly fixed payments</li>
+							<li>
+								Great for getting investors and creating
+								prototypes
+							</li>
+						</ul>
+						<button>Get started</button>
+					</div>
+				</section>
+				<section>
+					<h2>
+						Have a question? Want to discuss an idea? Feel free to
+						give us a call.
+					</h2>
+					<p>
+						Book an appointment where we can discuss your next big
+						idea. We're always here to help you with all of your
+						needs.
+					</p>
+					<button>Book a free consultation</button>
+				</section>
+			</div>
+			<footer>
+				<div>
+					<div>
+						<p>img placeholder</p>
+					</div>
+					<div>
+						<p>christopher@frameonesoftware.com</p>
+						<p>(604) 319-5219</p>
+						<p>Vancouver, BC</p>
+					</div>
+					<div>
+						<p>
+							©2022 Frame One Software Inc. All rights reserved.
+						</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						<ul>
+							<li>Home</li>
+							<li>People</li>
+							<li>Portfolio</li>
+							<li>Get in touch</li>
+						</ul>
+					</div>
+					<div>
+						<p>linkedin</p>
+						<p>git</p>
+					</div>
+				</div>
+			</footer>
+		</main>
+	);
+}
