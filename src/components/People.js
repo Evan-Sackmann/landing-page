d293b64f19b4c76bac0de932f11756caa2feeeeb
Evan-Sackmann <100897393+Evan-Sackmann@users.noreply.github.com>
2022-10-06 00:25:57 -0700

    basic layout

---
+++ src/components/People.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function People() {
+	return (
+		<div>
+			<h1>Meet our team</h1>
+			<p>
+				Frame One Software was founded by Christopher Powroznik and
+				Spencer Gray in 2019. They got their start building agriculture
+				software for local farmers around Vancouver Canada. Soon after
+				one of their earlier apps gained viral success, they utilized
+				that publicity to jump start Frame One Software and grow the
+				business.
+			</p>
+			<p>
+				Frame One Software's team was formed from the friends and
+				colleagues of founders Christopher Powroznik and Spencer Gray;
+				they have intentionally created a team that is full of skilled
+				developers capable of building any modern project. The staff is
+				composed entirely of Canadians living domestically. However, we
+				provide services in all timezones and are able to deliver
+				projects internationally.
+			</p>
+			<div></div>
+		</div>
+	);
+}
